37a79a6d1771fb5fc25cbafcfca9cf64fb0cd468
SohailaKandil <sohaila.kandil@ejust.edu.eg>
2025-01-14 22:52:48 +0200

    fixing pacman update func

--- run.py
+++ run.py
@@ -440,12 +440,13 @@ class GameController(object):
             ###update the pellet when eaten and delete the pellet rewards from it 
             if pellet.name == PELLET:
                 self.maze_map[2][pellet.tile[1]][pellet.tile[0]] -= PELLET_MAZE
+                self.updateScore(self.pellet_reward)
             elif pellet.name == POWERPELLET:
                 self.maze_map[3][pellet.tile[1]][pellet.tile[0]] -= 1
+                self.updateScore(self.pp_reward)
             ###
 
             self.pellets.numEaten += 1
-            self.updateScore(self.pellet_reward)
             ## update pellet points each time you eat a new one
             self.pellets.updatePoints()
             self.pellet_reward.value = pellet.points
@@ -524,22 +525,32 @@ class GameController(object):
 
 
 if __name__ == "__main__":
-    game = GameController(rlTraining=True , mode = SCARY_2_MODE , move_mode = DISCRETE_STEPS_MODE , clock_tick= 10 , pacman_lives=10 , maze_mode=RAND_MAZE, pac_pos_mode=RANDOM_PAC_POS)
+    game = GameController(rlTraining=True , mode = SCARY_2_MODE , move_mode = DISCRETE_STEPS_MODE , clock_tick= 10 , pacman_lives=2 , maze_mode=MAZE1 , pac_pos_mode=NORMAL_PAC_POS)
     done = False
     agent_direction=LEFT
 
     while True:
-        print(game.maze_map[4])
-        game.update(render=True )
-        #print(game.observation)
+        #agent_direction = random.randint(-2,2)
+        game.update(render=True ,  agent_direction = agent_direction)
+        # g = {4 : "red: " , 5 : "pink: "}
+        # for ghost in game.ghosts:
+        #     print(g[ghost.name] , ghost.direction , " " , game.maze_map[ghost.tile[1]][ghost.tile[0]]  , " ", ghost.direction == LEFT and game.pacman.tile[0] < ghost.tile[0])
         done = game.done
+        # print(done)
+        #print(game.score)
+        #print(len(game.pellets.pelletList))
+        #print("step: " , game.episode_steps)
         #print("direction: ", game.pacman.direction)
-
-        if agent_direction == LEFT:
-            agent_direction = RIGHT
-        elif agent_direction == RIGHT:
+        if game.pacman.tile == (6,23):
+            agent_direction = UP
+        if game.pacman.tile == (6,5):
             agent_direction = LEFT
-        
+        #print("*****************************")
+        # if agent_direction == LEFT:
+        #     agent_direction = RIGHT
+        # elif agent_direction == RIGHT:
+        #     agent_direction = LEFT
+
         # print ("done: " , game.done)
         # print("gameover: " , game.gameOver)
         # print("win: " , game.win)
